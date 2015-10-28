16c9907a6122521ea953eec5a0ff476d66ac4ba5
dcedena <david.cedena@gmail.com>
2015-10-28 17:25:36 +0100

    Modificado para almacenar solo un title y un link.

--- etsy/etsy/spiders/EtsyLinksMenu.py
+++ etsy/etsy/spiders/EtsyLinksMenu.py
@@ -1,8 +1,8 @@
-from scrapy.spider import BaseSpider
+import scrapy
 from scrapy.selector import HtmlXPathSelector
 from etsy.items import EtsyItem
 
-class EtsyLinksMenuSpider(BaseSpider):
+class EtsyLinksMenuSpider(scrapy.Spider):
 	name = "EtsyLinksMenu"
 	allowed_domains = ["etsy.com"]
 	start_urls = ["https://www.etsy.com/es/"]
@@ -11,13 +11,23 @@ class EtsyLinksMenuSpider(BaseSpider):
 		print '---> CEDENA <---'
 		items = []
 		for sel in response.xpath("//ul/li"):
-			item = EtsyItem()
-			item["title"] = sel.xpath('a/text()').extract()
-			item["link"] = sel.xpath('a/@href').extract()
+			titles = sel.xpath('a/text()').extract()
+			links = sel.xpath('a/@href').extract()
+			
+			first_title = ''
+			first_link = ''
 
-			tam_list = len(item['link'])
-			if(tam_list >= 1):
-				rel_url = item.get('link')[0]
-				if "?ref=catnav-" in rel_url:
+			if(len(titles) > 0):
+				first_title = titles[0]
+				first_title = first_title.replace('\n', '').strip()
+
+			if(len(links) > 0):
+				first_link = links[0]
+
+			if first_title and first_link:
+				if '?ref=catnav-' in first_link:
+					item = EtsyItem()
+					item['title'] = first_title
+					item['link'] = first_link
 					items.append(item)
 		return items
